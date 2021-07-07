d2b3cae1c3815ea13cab90eedf47e1592aa12bd9
Vladislav Gerasimov <vladislav.gerasimov@genestack.com>
2021-07-08 02:41:53 +0300

    Remove api gateway

--- wage-service/wage-service-app/src/main/java/org/fluffytiger/restservice/wages/WageEndpoint.java
+++ wage-service/wage-service-app/src/main/java/org/fluffytiger/restservice/wages/WageEndpoint.java
@@ -1,6 +1,10 @@
 package org.fluffytiger.restservice.wages;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +21,11 @@ public class WageEndpoint {
         this.eventProducerService = eventProducerService;
     }
 
-    @GetMapping("/ping")
-    public String ping() {
-        return "pong";
-    }
-
+    @Operation(summary = "Submit request with a wage without taxes.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Success"),
+        @ApiResponse(responseCode = "400", description = "Validation error")
+    })
     @PostMapping
     public void create(@Valid @RequestBody Wage wage) {
         eventProducerService.sendWageRequest(wage);
